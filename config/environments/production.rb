b22f8dc8699f3ed240d9b45248dffd4da7b5b68c
William Fish <william.michael.fish@gmail.com>
2021-04-30 13:59:25 +0100

    Fixes production logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
+  config.logger = ActiveSupport::Logger.new($stdout)
   config.log_level = :info
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Logstash.new
