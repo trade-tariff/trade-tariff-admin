9eae84d39845c2fbb4b95cbffef75244bb11f6a1
William Fish <william.michael.fish@gmail.com>
2021-05-14 11:10:38 +0100

    Make service uri dynamic!

--- lib/faraday_middleware/service_urls.rb
+++ lib/faraday_middleware/service_urls.rb
@@ -3,15 +3,10 @@ require 'faraday'
 module FaradayMiddleware
   class ServiceUrls < Faraday::Middleware
     def call(env)
+      service_uri = URI(TradeTariffAdmin::ServiceChooser.api_host)
       env.url.host = service_uri.host
       env.url.port = service_uri.port
       @app.call(env)
-    end
-
-    private
-
-    def service_uri
-      @service_uri ||= URI(TradeTariffAdmin::ServiceChooser.api_host)
     end
   end
 end
