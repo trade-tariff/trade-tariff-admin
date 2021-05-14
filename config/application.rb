3a093213a3a394ce5a93970d89e48ef92af17661
William Fish <william.michael.fish@gmail.com>
2021-05-14 12:23:10 +0100

    Require after included callback and autoload settings

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,6 @@ Bundler.require(*Rails.groups)
 
 APP_SLUG = 'trade-tariff-admin'.freeze
 
-require 'trade_tariff_admin'
-require 'trade_tariff_admin/service_chooser'
-
 module TradeTariffAdmin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -18,6 +15,8 @@ module TradeTariffAdmin
 
     config.autoloader = :classic
 
+    require 'trade_tariff_admin'
+    require 'trade_tariff_admin/service_chooser'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
