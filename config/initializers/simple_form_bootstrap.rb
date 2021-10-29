8ff8f6d3664194bb9fe611c8e6f21c4cc3f3f361
jebw <jeb@jdwilkins.co.uk>
2021-10-29 13:00:03 +0100

    HOTT-1032: Disabled browser validations
    We can provide better error messages using server side validation. Not the fields are still marked as required so continue to be accesibility friendly

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -8,6 +8,8 @@
 #
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
+  config.browser_validations = false
+
   config.error_notification_class = 'alert alert-danger'
   config.button_class = 'btn btn-default'
   config.boolean_label_class = nil
