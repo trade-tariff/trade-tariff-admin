e65d25e3d54f52e50b25be9377a90363ea67743d
jebw <jeb@jdwilkins.co.uk>
2021-11-09 14:04:30 +0000

    Converted rollbacks to GovUK style

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe ApplicationHelper do
 
     it { is_expected.to have_css 'div.govuk-breadcrumbs li.govuk-breadcrumbs__list-item a', count: 3 }
   end
+
+  describe '.govuk_form_for' do
+    subject do
+      govuk_form_for instance, url: '/ignore' do |f|
+        f.govuk_text_field :name
+      end
+    end
+
+    let :model do
+      Class.new do
+        include ActiveModel::Model
+
+        attr_accessor :name
+
+        def self.name
+          'Person'
+        end
+      end
+    end
+
+    let(:instance) { model.new name: 'Joe' }
+
+    it { is_expected.to have_css 'form .govuk-form-group label' }
+  end
 end
