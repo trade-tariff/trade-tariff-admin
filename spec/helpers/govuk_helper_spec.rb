8ff371b7c41a69f5ba9db0d779e8c733fd7820a3
jebw <jeb@jdwilkins.co.uk>
2021-11-09 16:34:59 +0000

    Converted Section notes

---
+++ spec/helpers/govuk_helper_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe GovukHelper do
+  let :model do
+    Class.new do
+      include ActiveModel::Model
+
+      attr_accessor :name
+
+      def self.name
+        'Person'
+      end
+    end
+  end
+
+  describe '.govuk_breadcrumbs' do
+    subject { govuk_breadcrumbs breadcrumbs }
+
+    let :breadcrumbs do
+      {
+        'first': '/first',
+        'second': '/second',
+        'third': '/third',
+      }
+    end
+
+    it { is_expected.to have_css 'div.govuk-breadcrumbs li.govuk-breadcrumbs__list-item a', count: 3 }
+  end
+
+  describe '.govuk_form_for' do
+    subject do
+      govuk_form_for instance, url: '/ignore' do |f|
+        f.govuk_text_field :name
+      end
+    end
+
+    let(:instance) { model.new name: 'Joe' }
+
+    it { is_expected.to have_css 'form .govuk-form-group label' }
+  end
+
+  describe 'submit_button_label' do
+    subject do
+      btn = nil
+      govuk_form_for(instance, url: '/') { |f| btn = submit_button_label(f) }
+      btn
+    end
+
+    before { allow(instance).to receive(:persisted?).and_return persisted }
+
+    let(:instance) { model.new }
+
+    context 'when new' do
+      let(:persisted) { false }
+
+      it { is_expected.to eql "Create #{model.name}" }
+    end
+
+    context 'when saved' do
+      let(:persisted) { true }
+
+      it { is_expected.to eql "Update #{model.name}" }
+    end
+  end
+
+  describe 'submit_and_back_buttons' do
+    subject do
+      govuk_form_for(model.new, url: '/') do |form|
+        submit_and_back_buttons form, back_link
+      end
+    end
+
+    let(:back_link) { root_path }
+
+    it { is_expected.to have_css 'button.govuk-button[type="submit"]' }
+    it { is_expected.to have_link 'Back', href: back_link }
+  end
+
+  describe 'govuk_markdown_area' do
+    subject do
+      govuk_form_for(model.new, url: '/') do |form|
+        govuk_markdown_area form, :name
+      end
+    end
+
+    it { is_expected.to have_css 'textarea.govuk-textarea' }
+    it { is_expected.to have_link 'Markdown guide' }
+  end
+end
