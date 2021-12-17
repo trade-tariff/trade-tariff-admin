5281a06e81595b2d2b21c52e7bcd68890ca70e94
jebw <jeb@jdwilkins.co.uk>
2021-12-17 14:39:31 +0000

    HOTT-1215 Removed no-longer used helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def nav_link(name, url, activator = '')
-    opts = {}
-    opts[:class] = 'active' if active_nav_link?(activator)
-
-    tag.li(opts) do
-      link_to name, url
-    end
-  end
-
   def active_nav_link?(activator)
     (activator.is_a?(String) && request.path.start_with?(activator)) ||
       (activator.is_a?(Regexp) && request.path =~ activator)
