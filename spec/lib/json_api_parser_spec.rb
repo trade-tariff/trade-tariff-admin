d9f967ad1134b90ed76fed8b7fbbddb007cd8548
William Fish <william.michael.fish@gmail.com>
2022-10-25 08:24:37 +0100

    HOTT-1531: Adds json_api_parser

---
+++ spec/lib/json_api_parser_spec.rb
@@ -0,0 +1,133 @@
+RSpec.describe JsonapiParser do
+  describe '#parse' do
+    let(:json) { JSON.parse(file_fixture("jsonapi/#{json_file}.json").read).deep_symbolize_keys }
+
+    context 'with singular resource' do
+      subject(:parsed) { described_class.new(json).parse }
+
+      context 'with valid' do
+        let(:json_file) { 'singular_no_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+      end
+
+      context 'without attributes field' do
+        let(:json_file) { 'singular_no_attributes' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.not_to include 'name' }
+      end
+
+      context 'with relationships' do
+        let(:json_file) { 'singular_with_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+
+        it 'maps relationship data' do
+          expect(parsed).to include parts: [{
+            resource_type: 'part',
+            resource_id: '456',
+            meta: { foo: 'bar' },
+            part_name: 'A part name',
+          }]
+        end
+      end
+
+      context 'with valid missing relationship' do
+        let(:json_file) { 'singular_valid_null_singular_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+        it { is_expected.to include part: nil }
+      end
+
+      context 'with missing relationships' do
+        let(:json_file) { 'singular_missing_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+        it { is_expected.to include parts: [{}] }
+      end
+
+      context 'with invalid relationships' do
+        let(:json_file) { 'singular_invalid_relationship' }
+
+        it 'raises a description exception' do
+          expect { parsed }.to raise_exception \
+            described_class::ParsingError,
+            "Error finding relationship 'parts': nil"
+        end
+      end
+    end
+
+    context 'with array resource' do
+      subject(:parsed) { described_class.new(json).parse.first }
+
+      context 'with valid' do
+        let(:json_file) { 'multiple_no_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+      end
+
+      context 'with relationships' do
+        let(:json_file) { 'multiple_with_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+
+        it 'maps relationship data' do
+          expect(parsed).to include parts: [{
+            resource_type: 'part',
+            resource_id: '456',
+            meta: { foo: 'bar' },
+            part_name: 'A part name',
+          }]
+        end
+      end
+
+      context 'with missing relationships' do
+        let(:json_file) { 'multiple_missing_relationship' }
+
+        it { is_expected.to include resource_type: 'mock_entity' }
+        it { is_expected.to include resource_id: '123' }
+        it { is_expected.to include meta: { foo: 'bar' } }
+        it { is_expected.to include name: 'Joe' }
+        it { is_expected.to include age: 21 }
+        it { is_expected.to include parts: [{}] }
+      end
+
+      context 'with invalid relationships' do
+        let(:json_file) { 'multiple_invalid_relationship' }
+
+        it 'raises a description exception' do
+          expect { parsed }.to raise_exception \
+            described_class::ParsingError,
+            "Error finding relationship 'parts': nil"
+        end
+      end
+    end
+  end
+end
