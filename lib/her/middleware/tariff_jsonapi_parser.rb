c60e494a8da92725e7f4cba1269a91ed6b5670cf
William Fish <william.michael.fish@gmail.com>
2022-10-25 08:27:40 +0100

    HOTT-1531: Move parser middleware to use JsonapiParser

--- lib/her/middleware/tariff_jsonapi_parser.rb
+++ lib/her/middleware/tariff_jsonapi_parser.rb
@@ -4,18 +4,8 @@ module Her
       def parse(body)
         json = parse_json(body)
 
-        included = json.fetch(:included, [])
-        primary_data = json.fetch(:data, {})
-
-        Array.wrap(primary_data).each do |resource|
-          next if resource.blank?
-
-          resource_relationships = resource.delete(:relationships) { {} }
-          resource[:attributes].merge!(populate_relationships(resource_relationships, included))
-        end
-
         {
-          data: primary_data || {},
+          data: JsonapiParser.new(json).parse,
           errors: json[:errors] || [],
           metadata: json[:meta] || {},
         }
@@ -32,27 +22,6 @@ module Her
                      else
                        parse(env[:body])
                      end
-      end
-
-      private
-
-      def populate_relationships(relationships, included)
-        return {} if included.empty?
-
-        {}.tap do |built|
-          relationships.each do |rel_name, linkage|
-            linkage_data = linkage.fetch(:data, {})
-            next if linkage_data.nil?
-
-            built_relationship = if linkage_data.is_a? Array
-                                   linkage_data.map { |l| included.detect { |i| i.values_at(:id, :type) == l.values_at(:id, :type) } }.compact
-                                 else
-                                   included.detect { |i| i && (i.values_at(:id, :type) == linkage_data.values_at(:id, :type)) }
-                                 end
-
-            built[rel_name] = built_relationship
-          end
-        end
       end
     end
   end
