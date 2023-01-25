8b5e891ab05e1b91510171528eaab2e30d13c49e
William Fish <william.michael.fish@gmail.com>
2023-01-25 13:15:50 +0000

    HOTT-1530: Removes balance_events_controller

--- app/controllers/balance_events_controller.rb
+++
@@ -1,7 +0,0 @@
-class BalanceEventsController < AuthenticatedController
-  def show
-    quota_definitions ||= QuotaOrderNumbers::QuotaDefinition.by_quota_order_number(params[:id])
-    
-    @current_quota_definition ||= quota_definitions.max_by(&:validity_end_date)
-  end
-end
