c1ca9b43bed8a9affb17583865adc6dff37310fa
William Fish <william.michael.fish@gmail.com>
2023-02-20 08:27:21 +0000

    HOTT-2375: Adjust heading search reference title

--- spec/factories/heading_factory.rb
+++ spec/factories/heading_factory.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :heading do
     goods_nomenclature_item_id { 10.times.map { Random.rand(1..9) }.join }
 
+    description { 'Live Horses, Asses, Mules And Hinnies' }
+
     trait :with_chapter do
       association :chapter, strategy: :build
 
--- spec/models/heading_spec.rb
+++ spec/models/heading_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Heading do
 
     it { is_expected.to match(/^headings-0201-references-\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z\.csv/) }
   end
+
+  describe '#reference_title' do
+    subject(:result) do
+      build(
+        :heading,
+        goods_nomenclature_item_id: '0101000000',
+        description: 'Live Horses',
+      ).reference_title
+    end
+
+    it { is_expected.to eq('heading 0101: Live Horses') }
+  end
 end
