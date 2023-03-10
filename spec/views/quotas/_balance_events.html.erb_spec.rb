97e452f336dc6c413fd5249dabee3431b28308c7
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2023-03-10 13:27:37 +0000

    HOTT-2808: Fix broken tests

--- spec/views/quotas/_balance_events.html.erb_spec.rb
+++ spec/views/quotas/_balance_events.html.erb_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe 'quotas/_balance_events' do
 
     it { is_expected.to have_css 'td', text: balance_event.last_import_date_in_allocation&.to_date&.to_formatted_s(:govuk_short) }
 
-    it { is_expected.to have_css 'td', text: balance_event.old_balance }
+    it { is_expected.to have_css 'td', text: number_with_precision(balance_event.old_balance, precision: 3, delimiter: ',') }
 
-    it { is_expected.to have_css 'td', text: balance_event.imported_amount }
+    it { is_expected.to have_css 'td', text: number_with_precision(balance_event.imported_amount, precision: 3, delimiter: ',') }
 
-    it { is_expected.to have_css 'td', text: balance_event.new_balance }
+    it { is_expected.to have_css 'td', text: number_with_precision(balance_event.new_balance, precision: 3, delimiter: ',') }
 
     it { is_expected.to have_css 'a', text: 'See the graph of quota balance events' }
   end
