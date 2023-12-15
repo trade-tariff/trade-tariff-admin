b3f369f8134514d60c58ef5742116012c66a4cd9
William Fish <william.michael.fish@gmail.com>
2023-12-15 16:50:15 +0000

    Revert "HOTT-4481: Update Rails from 7.0 to 7.1.2"

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security policy.
-# See the Securing Rails Applications Guide for more information:
-# https://guides.rubyonrails.org/security.html#content-security-policy-header
+# Define an application-wide content security policy
+# For further information see the following documentation
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.configure do
   config.content_security_policy do |policy|
@@ -15,11 +15,12 @@ Rails.application.configure do
     # Specify URI for violation reports
     policy.report_uri ENV['SENTRY_CSP_ENDPOINT'] if ENV['SENTRY_CSP_ENDPOINT'].present?
   end
-  
-  # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  # config.content_security_policy_nonce_directives = %w(script-src style-src)
 
-  # Report violations without enforcing the policy.
-  # config.content_security_policy_report_only = true
+  #   # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  # config.content_security_policy_nonce_directives = %w(script-src)
+  #
+  #   # Report CSP violations to a specified URI. See:
+  #   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+  #   config.content_security_policy_report_only = true
 end
