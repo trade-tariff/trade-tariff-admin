9f70e3259b4d81c90a6459b1bfc16649b3fa8981
Alexdesi <alexdesi@gmail.com>
2024-01-10 14:26:59 +0000

    HOTT-4481: Run "rails app:update" and fix configuration.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +24,12 @@ module TradeTariffAdmin
     config.action_dispatch.rescue_responses.merge!(
       'Faraday::ResourceNotFound' => :not_found,
     )
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
