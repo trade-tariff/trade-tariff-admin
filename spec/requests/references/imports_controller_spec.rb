946132e8af6b8d3e996e4652f62e9b81eef00bf4
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2024-01-31 13:05:51 +0000

    HOTT-4866: the 'import search references' button returns an error

--- spec/requests/references/imports_controller_spec.rb
+++ spec/requests/references/imports_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe References::ImportsController do
-  subject(:rendered) { create_user && make_request && response}
+  subject(:rendered) { create_user && make_request && response }
 
   let(:create_user) { create(:user, :hmrc_editor) }
 
@@ -9,6 +9,7 @@ RSpec.describe References::ImportsController do
     before do
       create_list(:import_task, 5)
     end
+
     let(:make_request) { get references_import_path }
 
     it { is_expected.to have_http_status :success }
@@ -16,7 +17,7 @@ RSpec.describe References::ImportsController do
 
   describe 'POST #create' do
     context 'with valid parameters' do
-      let(:make_request) { post references_import_path, params: { import_task: { file: fixture_file_upload('search_references.csv', 'text/csv') }}}
+      let(:make_request) { post references_import_path, params: { import_task: { file: fixture_file_upload('search_references.csv', 'text/csv') } } }
 
       it { is_expected.to have_http_status :redirect }
     end
