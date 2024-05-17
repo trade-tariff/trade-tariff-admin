116fec61ee22a8d6b91b08d0ae6e1cebe29f57d8
rasika.abeyrathna <rasikasri@gmail.com>
2024-05-17 12:43:49 +0100

    GL-371: Add admin UI to create/edit pseudo exemptions

--- app/controllers/green_lanes/exemptions_controller.rb
+++ app/controllers/green_lanes/exemptions_controller.rb
@@ -7,5 +7,44 @@ module GreenLanes
     def index
       @exemptions = GreenLanes::Exemption.all.fetch
     end
+
+    def new
+      @exemption = GreenLanes::Exemption.new
+    end
+
+    def create
+      @exemption = GreenLanes::Exemption.new(ex_params)
+
+      if @exemption.valid? && @exemption.save
+        redirect_to green_lanes_exemptions_path, notice: 'Exemption created'
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @exemption = GreenLanes::Exemption.find(params[:id])
+    end
+
+    def update
+      @exemption = GreenLanes::Exemption.find(params[:id])
+      @exemption.attributes = ex_params
+
+      if @exemption.valid? && @exemption.save
+        redirect_to green_lanes_exemptions_path, notice: 'Exemption updated'
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def ex_params
+      params.require(:exemption).permit(
+        :code,
+        :description,
+      )
+    end
+
   end
 end
--- spec/requests/green_lanes/exemptions_controller_spec.rb
+++ spec/requests/green_lanes/exemptions_controller_spec.rb
@@ -19,4 +19,81 @@ RSpec.describe GreenLanes::ExemptionsController do
     it { is_expected.to have_http_status :success }
     it { is_expected.not_to include 'div.current-service' }
   end
+
+  describe 'GET #new' do
+    let(:make_request) { get new_green_lanes_exemption_path }
+
+    it { is_expected.to have_http_status :ok }
+    it { is_expected.not_to include 'div.current-service' }
+  end
+
+  describe 'POST #create' do
+    before do
+      stub_api_request('/admin/green_lanes/exemptions', :post).to_return create_response
+    end
+
+    let :make_request do
+      post green_lanes_exemptions_path,
+           params: { exemption: ex_params }
+    end
+
+    context 'with valid item' do
+      let(:ex_params) { exemption.attributes.without(:id) }
+      let(:create_response) { webmock_response(:created, exemption.attributes) }
+
+      it { is_expected.to redirect_to green_lanes_exemptions_path }
+    end
+
+    context 'with invalid item' do
+      let(:ex_params) { exemption.attributes.without(:id, :code) }
+      let(:create_response) { webmock_response(:error, code: "can't be blank'") }
+
+      it { is_expected.to have_http_status :ok }
+      it { is_expected.to have_attributes body: /can.+t be blank/ }
+      it { is_expected.not_to include 'div.current-service' }
+    end
+  end
+
+  describe 'GET #edit' do
+    before do
+      stub_api_request("/admin/green_lanes/exemptions/#{exemption.id}")
+        .and_return jsonapi_response(:exemption, exemption.attributes)
+    end
+
+    let(:make_request) { get edit_green_lanes_exemption_path(exemption) }
+
+    it { is_expected.to have_http_status :success }
+    it { is_expected.not_to include 'div.current-service' }
+  end
+
+  describe 'PATCH #update' do
+    before do
+      stub_api_request("/admin/green_lanes/exemptions/#{exemption.id}")
+        .and_return jsonapi_response(:exemption, exemption.attributes)
+
+      stub_api_request("/admin/green_lanes/exemptions/#{exemption.id}", :patch)
+        .and_return patch_response
+    end
+
+    let :make_request do
+      patch green_lanes_exemption_path(exemption),
+            params: { exemption: exemption.attributes.merge(description: new_desc) }
+    end
+
+    context 'with valid change' do
+      let(:new_desc) { 'New description' }
+      let(:patch_response) { webmock_response :updated, "/admin/green_lanes/exemptions/#{exemption.id}" }
+
+      it { is_expected.to redirect_to green_lanes_exemptions_path }
+    end
+
+    context 'with invalid change' do
+      let(:new_desc) { nil }
+      let(:patch_response) { webmock_response :error, description: "can't be blank" }
+
+      it { is_expected.to have_http_status :ok }
+      it { is_expected.to have_attributes body: /can.+t be blank/ }
+      it { is_expected.not_to include 'div.current-service' }
+    end
+  end
 end
