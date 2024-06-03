e0f4ac9be601544041dff972f944dae6e99255ef
rasika.abeyrathna <rasikasri@gmail.com>
2024-06-03 16:18:46 +0100

    GL-370: Add admin UI to List pseudo measures

--- app/models/green_lanes/goods_nomenclature.rb
+++ app/models/green_lanes/goods_nomenclature.rb
@@ -2,6 +2,6 @@ module GreenLanes
   class GoodsNomenclature
     include Her::JsonApi::Model
 
-    attributes :description, :goods_nomenclature_item_id
+    attributes :description, :goods_nomenclature_item_id, :goods_nomenclature_sid
   end
 end
