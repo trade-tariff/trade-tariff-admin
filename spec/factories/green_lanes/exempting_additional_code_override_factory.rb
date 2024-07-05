3080d6ba3ce5c60663798a48c73daa4689181fac
rasika.abeyrathna <rasikasri@gmail.com>
2024-07-05 15:24:50 +0100

    GL-707: Add support to whitelist additional codes as exemptions in the admin

--- spec/factories/green_lanes/exempting_additional_code_override_factory.rb
+++ spec/factories/green_lanes/exempting_additional_code_override_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :exempting_additional_code_override, class: 'GreenLanes::ExemptingAdditionalCodeOverride' do
     sequence(:id) { |n| n }
     additional_code_type_id { 'C' }
-    additional_code      { '499' }
+    additional_code { '499' }
     created_at { 2.days.ago.to_date }
     updated_at { nil }
   end
