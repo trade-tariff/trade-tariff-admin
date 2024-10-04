0e9002d92cbd430a772565acebcf65779707b905
rasika.abeyrathna <rasikasri@gmail.com>
2024-10-04 17:27:34 +0100

    GL-1065: Implement Sorting Functionality on Category Assessments Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     (activator.is_a?(String) && request.path.start_with?(activator)) ||
       (activator.is_a?(Regexp) && request.path =~ activator)
   end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, { sort: column, direction: direction, page: params[:page], filters: params[:filters] }
+  end
 end
