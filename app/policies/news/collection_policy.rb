a9ecbb924c55f5d5001e608a8afc894b989bc2f2
William Fish <william.michael.fish@gmail.com>
2025-04-04 12:44:33 +0100

    BAU: Removes unused authorisation checks

--- app/policies/news/collection_policy.rb
+++ app/policies/news/collection_policy.rb
@@ -1,5 +1,5 @@
 News::CollectionPolicy = Struct.new(:user, :news_collection) do
   def edit?
-    user.gds_editor? || user.hmrc_editor? || user.hmrc_admin? || !TradeTariffAdmin.authenticate_with_sso?
+    user.gds_editor? || user.hmrc_editor? || user.hmrc_admin?
   end
 end
--- app/policies/section_note_policy.rb
+++ app/policies/section_note_policy.rb
@@ -1,5 +1,5 @@
 SectionNotePolicy = Struct.new(:user, :section_note) do
   def edit?
-    user.gds_editor? || user.hmrc_editor? || user.hmrc_admin? || !TradeTariffAdmin.authenticate_with_sso?
+    user.gds_editor? || user.hmrc_editor? || user.hmrc_admin?
   end
 end
