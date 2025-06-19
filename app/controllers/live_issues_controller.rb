3856cd04d89f9edbb139e14ddafca80a77268641
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-06-19 16:12:07 +0100

    Create new live issue through form

--- app/controllers/live_issues_controller.rb
+++ app/controllers/live_issues_controller.rb
@@ -7,4 +7,33 @@ class LiveIssuesController < AuthenticatedController
     render 'index'
   end
 
+  def new
+    @live_issue = LiveIssue.new
+    render 'new'
+  end
+
+  def create
+    @live_issue = LiveIssue.new(live_issue_params)
+    @live_issue.save
+
+    if @live_issue.errors.none?
+      redirect_to live_issues_path, notice: 'Live issue created'
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def live_issue_params
+    params.require(:live_issue).permit(
+      :title,
+      :description,
+      :suggested_action,
+      :date_discovered,
+      :date_resolved,
+      :commodities,
+      status: []
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -78,7 +78,7 @@ Rails.application.routes.draw do
   resources :news_collections, only: %i[index edit update create new]
   resources :reports, only: %i[index show]
   resources :balance_events, only: %i[show]
-  resources :live_issues, only: %i[index]
+  resources :live_issues, only: %i[index create new]
 
   resources :quotas, only: %i[new show] do
     collection do
