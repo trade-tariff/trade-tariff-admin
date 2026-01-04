8915a3f1c949f506f33a0aaa8944f7816ac92bc0
Chris Rose <chrisroseuk@gmail.com>
2026-01-04 18:29:21 +0000

    HMRC-1822: Ensure basic auth user always has technical_operator role
    - Remove basic_auth_user from seeds (created automatically on login)
    - Update basic_auth_user! to always set TECHNICAL_OPERATOR role
    - Remove unused system_user! method
    - Update local nonprod database with correct user role

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# Basic Auth User - persisted static user for basic authentication
-# This user has technical_operator role for admin access
-User.basic_auth_user!.tap do |user|
-  user.role = User::TECHNICAL_OPERATOR
-  user.save!
-end
-
 # Winston user (legacy)
 User.find_or_initialize_by(uid: 'winston', email: 'winston@alphagov.co.uk').tap do |user|
   user.name = 'Winston'
