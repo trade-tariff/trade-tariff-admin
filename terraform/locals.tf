b07310f2f65a1ca88df6afd3999be87b38431be4
William Fish <william.michael.fish@gmail.com>
2026-01-12 10:07:41 +0000

    BAU: Make sure migrations run

---
+++ terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  secret_value = try(data.aws_secretsmanager_secret_version.this.secret_string, "{}")
+  secret_map   = jsondecode(local.secret_value)
+  secret_env_vars = [
+    for key, value in local.secret_map : {
+      name  = key
+      value = value
+    }
+  ]
+}
