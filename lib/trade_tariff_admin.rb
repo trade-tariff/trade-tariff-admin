e1ff08cbe7cc539619065f8d251a97aba9f954cd
William Fish <william.michael.fish@gmail.com>
2026-01-19 17:05:41 +0000

    BAU: Handle different environment cookies

--- lib/trade_tariff_admin.rb
+++ lib/trade_tariff_admin.rb
@@ -107,5 +107,26 @@ module TradeTariffAdmin
         URI.parse(domain).host.sub("admin.", "")
       end
     end
+
+    def environment
+      ENV.fetch("ENVIRONMENT", "production")
+    end
+
+    def id_token_cookie_name
+      cookie_name_for("id_token")
+    end
+
+    def refresh_token_cookie_name
+      cookie_name_for("refresh_token")
+    end
+
+    def cookie_name_for(base_name)
+      case environment
+      when "production"
+        base_name
+      else
+        "#{environment}_#{base_name}"
+      end.to_sym
+    end
   end
 end
