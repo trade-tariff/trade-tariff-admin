e4a2187c63b9fcdd26a23b044dabdad3cf35fa52
rasika.abeyrathna <rasikasri@gmail.com>
2026-03-26 11:56:47 +0000

    HMRC-1965

--- config/puma.rb
+++ config/puma.rb
@@ -26,9 +26,10 @@
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
+rails_env = ENV.fetch("RAILS_ENV", "development")
 
 # Explicit HTTP bind,  default is 3000.
-if Rails.env.development?
+if rails_env == "development"
   bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 end
 
