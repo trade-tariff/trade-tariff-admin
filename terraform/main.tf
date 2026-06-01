7d7301c6ed1ec38a98cc875ac3453d22cf2a8003
Aigul Djon <aigul.djon@gmail.com>
2026-06-01 14:31:18 +0100

    feat(alarms): route ecs capacity and cpu alarms to production-observability channel

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "service" {
-  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v3.0.1"
+  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v3.1.0"
 
   region = var.region
 
@@ -33,5 +33,6 @@ module "service" {
 
   service_environment_config = local.admin_service_env_vars
 
-  sns_topic_arns = [data.aws_sns_topic.slack_topic.arn]
+  sns_topic_arns               = [data.aws_sns_topic.slack_topic.arn]
+  observability_sns_topic_arns = var.enable_observability_alerts ? [data.aws_sns_topic.slack_observability_topic[0].arn] : null
 }
