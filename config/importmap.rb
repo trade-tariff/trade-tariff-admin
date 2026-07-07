7ea5a584a82db88dcd89e7d333b78a90d8be47c2
Amber Cronin <amber@amber.vision>
2026-07-07 11:44:42 +0100

    chore: bump vendored govuk-frontend inline with dependabot change

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin "application"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-pin "govuk-frontend" # @6.2.0
+pin "govuk-frontend" # @6.3.0
 pin "accessible-autocomplete" # @3.0.1
 pin "chart.js" # @4.5.1 jsdelivr +esm
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.4.0
